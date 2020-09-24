a23abdadcbbd3069366b6f43abb275b080b9983c
degroot12 <degroot12@gmail.com>
2020-09-24 10:14:45 +0200

    Nu met spaties, return en ;

--- Temperatuur.js
+++ Temperatuur.js
@@ -1,10 +1,10 @@
 function celciusFahrenheit(temp, waarde) {
     if (waarde === 'C') {
-    console.log(temp * 9/5 + 32 + ' F')
+        return (temp * 9/5 + 32 + ' F');
     } else if (waarde === 'F') {
-        console.log(Math.round((temp - 32) / 1.8) + ' C');
+        return (Math.round((temp - 32) / 1.8) + ' C');
     } else {
-        console.log('Foute waardes ingevoerd.')
+        return ('Foute waardes ingevoerd.');
     }
 };
-celciusFahrenheit(35.5, 'C')
+console.log(celciusFahrenheit(76.5, 'F'));
